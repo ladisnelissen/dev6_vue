a50d745f13e5ce2692d1fc59b4acc31d0ba99b62
Ladis Nelissen <ladisnelissen@hotmail.com>
2022-11-21 14:06:40 +0100

    feat: post message test

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,14 +1,56 @@
 <script setup>
 import { ref, onMounted, reactive } from 'vue';
 
+let users = reactive({
+    data: [],
+})
 
+let comments = reactive({
+    data: [],
+})
+
+onMounted(() => {
+   fetch("https://lab5-p379.onrender.com/api/v1/messages/")
+   .then(res => res.json())
+   .then(data => {
+         //loop through data and get users
+         for (let i = 0; i < data.length; i++) {
+             users.data.push(data[i].user);
+         }
+
+         for (let i = 0; i < data.length; i++) {
+             comments.data.push(data[i].text);
+         }
+      })
+   }) 
+
+   //send message
+    const addComment = () => {
+        // get message from input
+        let message = document.getElementById("message").value;
+        let data = {
+            user: "Ladis",
+            text: message
+        }
+        fetch("https://lab5-p379.onrender.com/api/v1/messages/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        })
+    }
 
 
 </script>
 
 <template>
     <div>
-        <input type="text" v-model="message" @keyup.enter="sendMessage">
+    <input id="message" type="text" v-model="message" @keyup.enter="sendMessage">
     <button @click="addComment">Add Comment</button>
     </div>
   
