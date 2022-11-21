f1acf998bea614eafc041de74228e2ed286bad9f
Ladis Nelissen <ladisnelissen@hotmail.com>
2022-11-21 13:01:30 +0100

    feat: fetch data from api

--- src/components/Comments.vue
+++ src/components/Comments.vue
@@ -2,6 +2,21 @@
 import { ref, onMounted, reactive } from 'vue';
 let user = ref("");
 let text = ref("");
+
+
+
+onMounted(() => {
+   fetch("https://lab5-p379.onrender.com/api/v1/messages/")
+   .then(res => res.json())
+   .then(data => {
+      user.value = data.user;
+      text.value = data.text;
+
+      console.log(data[0].user);
+   })
+ 
+   });
+
 </script>
 
 <template>
