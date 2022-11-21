a6f13f7ebf59f279e5506d3b29390040d9383dbb
Ladis Nelissen <ladisnelissen@hotmail.com>
2022-11-21 14:02:47 +0100

    feat: add chatbox component to app

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import VideoPlayer from './components/VideoPlayer.vue';
 import VideoDetails from './components/VideoDetails.vue';
 import Comments from './components/Comments.vue';
+import Chat from './components/Chat.vue';
 
 </script>
 
@@ -11,6 +12,7 @@ import Comments from './components/Comments.vue';
     <div.sidebar>
       <VideoDetails />
       <Comments />
+      <Chat />
     </div.sidebar>
     
    
