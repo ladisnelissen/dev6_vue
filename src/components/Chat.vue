bdbd6e4126a4bb54ea178d18e27260e827d69541
Ladis Nelissen <ladisnelissen@hotmail.com>
2022-11-21 14:02:39 +0100

    feat: style chatbox

---
+++ src/components/Chat.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, onMounted, reactive } from 'vue';
+
+
+
+
+</script>
+
+<template>
+    <div>
+        <input type="text" v-model="message" @keyup.enter="sendMessage">
+    <button @click="addComment">Add Comment</button>
+    </div>
+  
+  
+</template>
+
+<style scoped>
+    div {
+        padding: 0 1rem;
+        margin-top: 10px;
+    }
+</style>
